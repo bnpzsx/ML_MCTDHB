f0049fdd759178de32fadde83ce0d516dc6ce3f2
bnpzsx <1150892585@qq.com>
2019-08-12 20:51:03 +0800

    ns排列及反求序号

--- utils.py
+++ utils.py
@@ -42,3 +42,33 @@ def num_combination_repetition(n,m):
     'H(n,m) 从n个不同的元素取出m个元素的允许重复的组合总数'
     assert n>=1 ,'组合参数不符合要求, %d<1'%(n)
     return num_combination(n+m-1,m)
+
+def ns_distribution(n,m):
+    'n个粒子m个轨道的 number state in a bosonic node'
+    #`n+1`个位置插`m-1`个板,板位置差为ns
+    pos=combinations_with_replacement(range(n+1),r=m-1)
+    pos=np.array(list(pos))
+    c=pos[:,1:]-pos[:,:-1]
+    t=n*np.ones((1,len(pos)),int)-pos[:,-1]
+    ns=np.hstack((pos[:,0:1],c,t.T))
+    return ns[::-1]
+
+def ns_index(ns):
+    '得到`ns`在一组number state中的序号'
+    #先逆变换为插板位置,再变为序号
+    n=sum(ns)
+    m=len(ns)
+    num_ns=num_combination_repetition(n+1,m-1)
+    a=[]
+    t=0
+    for i in range(m-1):
+        t=t+ns[i]
+        a.append(t)
+    index=0
+    t=0
+    for i,j in enumerate(a):
+        for l,k in enumerate(range(j-t)):
+            w=num_combination_repetition(n+1-k-t,m-2-i)
+            index+=w    
+        t=j
+    return num_ns-index-1
